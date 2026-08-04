e40c789661e30ed64eda88c9b490c32329c9f7cf
coreyfmiller <coreyfmiller@gmail.com>
2026-08-04 16:20:25 -0300

    Add gitignore, remove .next cache

--- .next/dev/static/chunks/_1anvha4._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_1mojsay._.js","static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_12mgjs2._.js","static/chunks/1jh5_next_dist_compiled_next-devtools_index_1g74ild.js","static/chunks/1jh5_next_dist_compiled_react-dom_1vn7l-x._.js","static/chunks/1jh5_next_dist_compiled_react-server-dom-turbopack_0nmdlbf._.js","static/chunks/1jh5_next_dist_compiled_1xdqdxj._.js","static/chunks/1jh5_next_dist_client_17i-e4b._.js","static/chunks/1jh5_next_dist_1uivv-j._.js","static/chunks/0vvp_@swc_helpers_cjs_0vb02wp._.js"],
-    source: "entry"
-});
--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/1jh5_next_dist_1151wf6._.js","static/chunks/022k_three_build_three_core_112lsxf.js","static/chunks/022k_three_build_three_module_0gs89bd.js","static/chunks/022k_three_build_three_module_05j5bra.js","static/chunks/110j_@react-three_fiber_dist_1rnci2b._.js","static/chunks/node_modules__pnpm_0jpgwa5._.js","static/chunks/_20b-nf1._.js"],
-    source: "entry"
-});
